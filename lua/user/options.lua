abb6da0e33cd3805c56f1b209bf6d59e1f179c52
Vishnu Ganesan <vishnukg@gmail.com>
2022-12-28 14:58:21 +1100

    set block cursor in insert mode

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -33,6 +33,7 @@ vim.opt.wrap = false                            -- display lines as one long lin
 vim.opt.scrolloff = 8                           -- is one of my fav
 vim.opt.sidescrolloff = 8
 vim.opt.guifont = "monospace:h17"               -- the font used in graphical neovim applications
+vim.opt.guicursor = ""                          -- Set block cursor in insert mode
 
 vim.opt.shortmess:append "c"
 
