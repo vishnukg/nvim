18d5411c32bd8c3086120680692bcb237d8cc067
Vishnu Ganesan <vishnukg@gmail.com>
2022-12-28 14:40:52 +1100

    Added shortcut for saving across buffers

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -5,3 +5,18 @@ local term_opts = { silent = true }
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 keymap("n", "<C-g>", ":Lex 30<cr>", opts)
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Insert --
+-- Press jk fast to enter
+keymap("i", "jk", "<ESC>", opts)
+
+-- Shortcut for saving all changed files in normal mode
+keymap("n","<leader><leader>", ":wa<cr>",opts)
