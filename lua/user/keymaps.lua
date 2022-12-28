426e0bac7483c5c10dc983d2c24651534000e968
Vishnu Ganesan <vishnukg@gmail.com>
2022-12-28 15:34:44 +1100

    Added more keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -30,3 +30,13 @@ keymap("n","<Left>",":echoe \"Use h\"<cr>",opts)
 keymap("n","<Right>",":echoe \"Use l\"<cr>",opts)
 keymap("n","<Up>",":echoe \"Use k\"<cr>",opts)
 keymap("n","<Down>",":echoe \"Use j\"<cr>",opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
