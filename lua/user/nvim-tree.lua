3a39f53aed1451c0f19fb98f5fedf57e8c5455cf
Vishnu Ganesan <vishnukg@gmail.com>
2022-12-29 00:20:34 +1100

    Updated nvim

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -1,3 +1,7 @@
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 
