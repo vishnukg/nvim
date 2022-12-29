c3a02af2acf1be1499e814f6a27b2b6b1bdb6c29
Vishnu Ganesan <vishnukg@gmail.com>
2022-12-30 00:02:34 +1100

    Adding rustfmt to null-ls

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -19,6 +19,7 @@ null_ls.setup({
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
 		formatting.gofmt,
+		formatting.rustfmt,
 		diagnostics.flake8,
 		diagnostics.golangci_lint,
 	},
