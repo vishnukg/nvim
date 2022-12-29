ae6f57f5cd7f38352bd6102cfba55928df151306
Vishnu Ganesan <vishnukg@gmail.com>
2022-12-29 19:53:25 +1100

    Added auto formatting using null-ls

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -9,11 +9,14 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-    debug = false,
-    sources = {
-        formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-        formatting.black.with({ extra_args = { "--fast" } }),
-        formatting.stylua,
-        diagnostics.flake8
-    },
+	debug = false,
+	sources = {
+		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		formatting.black.with({ extra_args = { "--fast" } }),
+		formatting.stylua,
+		diagnostics.flake8,
+	},
 })
+
+-- Auto formatting on save
+vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format{async = true}]])
