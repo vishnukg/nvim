9ff29e08c9cabfa604241165974b5d5213586a58
Vishnu Ganesan <vishnukg@gmail.com>
2022-12-29 13:25:46 +1100

    Enabled diagnostic for null-ls

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -9,11 +9,11 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
-    -- diagnostics.flake8
-	},
+    debug = false,
+    sources = {
+        formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+        formatting.black.with({ extra_args = { "--fast" } }),
+        formatting.stylua,
+        diagnostics.flake8
+    },
 })
