33e1155aa64381a481221ef0760f98e5294415fa
Vishnu Ganesan <vishnukg@gmail.com>
2022-12-29 18:58:49 +1100

    Updating colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -5,11 +5,18 @@ if not status_ok then
   return
 end
 
-local api = vim.api
-api.nvim_set_hl(0, "Normal", { bg = "none",fg = "none" })
-api.nvim_set_hl(0, "NormalFloat", { bg = "none", fg= "none" })
-api.nvim_set_hl(0, "CursorLine",{bg = "#464d53", fg="None"})
-api.nvim_set_hl(0, "StatusLine",{bg = "none", fg="None"})
-api.nvim_set_hl(0, "Visual",{bg = "#4da9ff", fg="None"})
+-- Transparency setting
+vim.api.nvim_set_hl(0, "Normal", { bg = "none",fg ="none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none",fg= "none"})
+vim.api.nvim_set_hl(0, "StatusLine",{bg = "none", fg="None"})
+vim.api.nvim_set_hl(0, "Folded",{bg = "none", fg="None"})
+vim.api.nvim_set_hl(0, "SpecialKey",{bg = "none", fg="None"})
+vim.api.nvim_set_hl(0, "VertSplit",{bg = "none", fg="None"})
+vim.api.nvim_set_hl(0, "SignColumn",{bg = "none", fg="None"})
+vim.api.nvim_set_hl(0, "NonText",{bg = "none", fg="None"})
 
+
+
+vim.api.nvim_set_hl(0, "Visual",{bg = "#4da9ff", fg="None"})
+vim.api.nvim_set_hl(0, "CursorLine",{bg = "#464d53", fg="None"})
 
