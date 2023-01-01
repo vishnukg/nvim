1f4487f2b5ae3490266dda50a862d4e39001d402
Vishnu Ganesan <vishnukg@gmail.com>
2023-01-01 17:14:20 +1100

    Using go imports instead of go fmt in null-ls

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -21,7 +21,7 @@ null_ls.setup({
 		formatting.prettier,
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-		formatting.gofmt,
+		formatting.goimports,
 		formatting.rustfmt,
 		formatting.csharpier,
 		diagnostics.flake8,
