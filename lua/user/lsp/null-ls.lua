abfdb8347c6b30cb2f6b8c438ec7c56b12a92138
Vishnu Ganesan <vishnukg@gmail.com>
2023-01-01 15:47:29 +1100

    Updated format on save for null lsp

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -8,6 +8,9 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
+-- LspFormatting
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 -- This is where you add more formatters. If you want to add a formatter, first install the binary (Ex. stylua or black)
 -- to the system and then setup it up here so null-ls can use it behind the scenes.
 -- Behind the scenes its uses lua vim.lsp.buf.format to autoformat
@@ -24,7 +27,20 @@ null_ls.setup({
 		diagnostics.flake8,
 		diagnostics.golangci_lint,
 	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
 
 -- Auto formatting on save
-vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format({async = true})]])
+-- vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format({async = true})]])
