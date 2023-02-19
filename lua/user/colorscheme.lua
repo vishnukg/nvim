ebaa04fa2f1c269e5f9b1eb2b4ee6aea9c6cab12
Vishnu Ganesan <vishnukg@gmail.com>
2023-02-19 23:31:42 +1100

    Set popup menu selection background color

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -10,18 +10,14 @@ local c = require("vscode.colors")
 require("vscode").setup({
 	-- Enable transparent background
 	transparent = true,
-
 	-- Enable italic comment
 	italic_comments = true,
-
 	-- Disable nvim-tree background color
 	disable_nvimtree_bg = true,
-
 	-- Override colors (see ./lua/vscode/colors.lua)
 	color_overrides = {
 		vscLineNumber = "#464d53",
 	},
-
 	-- Override highlight groups (see ./lua/vscode/theme.lua)
 	group_overrides = {
 		-- this supports the same val table as vim.api.nvim_set_hl
@@ -41,3 +37,7 @@ require("vscode").setup({
 
 vim.api.nvim_set_hl(0, "Visual", { bg = "#4da9ff", fg = "None" })
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "#464d53", fg = "None" })
+
+-- hi Pmenu bg=color to set popup menu background
+-- hi PmenuSel bg=color to set popup menu selection background
+vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#4da9ff", fg = "None" })
