9303fc65cc12289dd1fb81c5fbfb48a96c8670dd
Vishnu Ganesan <vishnukg@gmail.com>
2023-04-09 19:39:50 +1000

    Updated packer command for numToStr/Comment.nvim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -3,16 +3,16 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
-	print("Installing packer close and reopen Neovim...")
-	vim.cmd([[packadd packer.nvim]])
+    PACKER_BOOTSTRAP = fn.system({
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+    })
+    print("Installing packer close and reopen Neovim...")
+    vim.cmd([[packadd packer.nvim]])
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -26,86 +26,86 @@ vim.cmd([[
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-	return
+    return
 end
 
 -- Have packer use a popup window
 packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
+    display = {
+        open_fn = function()
+            return require("packer.util").float({ border = "rounded" })
+        end,
+    },
 })
 
 -- Install your plugins here
 return packer.startup(function(use)
-	-- Basic plugins here
-	use("wbthomason/packer.nvim") -- Have packer manage itself
-	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
-	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
-	use({
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.0",
-		-- or                            , branch = '0.1.x',
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
-	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
-	use("nvim-treesitter/playground")
-	use("mbbill/undotree")
-	use("tpope/vim-fugitive")
-	use("kyazdani42/nvim-web-devicons")
-	use("kyazdani42/nvim-tree.lua")
-	use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and treesitter
-	use("akinsho/toggleterm.nvim")
+    -- Basic plugins here
+    use("wbthomason/packer.nvim") -- Have packer manage itself
+    use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
+    use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
+    use({
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.0",
+        -- or                            , branch = '0.1.x',
+        requires = { { "nvim-lua/plenary.nvim" } },
+    })
+    use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+    use("nvim-treesitter/playground")
+    use("mbbill/undotree")
+    use("tpope/vim-fugitive")
+    use("kyazdani42/nvim-web-devicons")
+    use("kyazdani42/nvim-tree.lua")
+    use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and treesitter
+    use("akinsho/toggleterm.nvim")
 
-	-- Lualine
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-	})
+    -- Lualine
+    use({
+        "nvim-lualine/lualine.nvim",
+        requires = { "kyazdani42/nvim-web-devicons", opt = true },
+    })
 
-	-- Colorscheme
-	use("lunarvim/darkplus.nvim")
-	use("Mofiqul/vscode.nvim")
+    -- Colorscheme
+    use("lunarvim/darkplus.nvim")
+    use("Mofiqul/vscode.nvim")
 
-	-- Commenting code
-	use({
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup()
-		end,
-	})
+    -- Commenting code
+    use {
+        "numToStr/Comment.nvim",
+        config = function()
+            require("Comment").setup()
+        end
+    }
 
-	-- cmp plugins
-	use("hrsh7th/nvim-cmp") -- The completion plugin
-	use("hrsh7th/cmp-buffer") -- buffer completions
-	use("hrsh7th/cmp-path") -- path completions
-	use("hrsh7th/cmp-cmdline") -- cmdline completions
-	use("saadparwaiz1/cmp_luasnip") -- snippet completions
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-nvim-lua")
+    -- cmp plugins
+    use("hrsh7th/nvim-cmp")      -- The completion plugin
+    use("hrsh7th/cmp-buffer")    -- buffer completions
+    use("hrsh7th/cmp-path")      -- path completions
+    use("hrsh7th/cmp-cmdline")   -- cmdline completions
+    use("saadparwaiz1/cmp_luasnip") -- snippet completions
+    use("hrsh7th/cmp-nvim-lsp")
+    use("hrsh7th/cmp-nvim-lua")
 
-	-- snippets
-	use("L3MON4D3/LuaSnip") --snippet engine
-	use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
+    -- snippets
+    use("L3MON4D3/LuaSnip")          --snippet engine
+    use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
 
-	-- LSP
-	use("neovim/nvim-lspconfig") -- enable LSP
-	use("williamboman/mason.nvim") -- simple to use language server installer
-	use("williamboman/mason-lspconfig.nvim") -- simple to use language server installer
-	use("jose-elias-alvarez/null-ls.nvim") -- LSP diagnostics and code actions
-	use("j-hui/fidget.nvim") -- LSP progress status
+    -- LSP
+    use("neovim/nvim-lspconfig")          -- enable LSP
+    use("williamboman/mason.nvim")        -- simple to use language server installer
+    use("williamboman/mason-lspconfig.nvim") -- simple to use language server installer
+    use("jose-elias-alvarez/null-ls.nvim") -- LSP diagnostics and code actions
+    use("j-hui/fidget.nvim")              -- LSP progress status
 
-	-- Git
-	use("lewis6991/gitsigns.nvim")
+    -- Git
+    use("lewis6991/gitsigns.nvim")
 
-	-- Search and Replace
-	use("windwp/nvim-spectre")
+    -- Search and Replace
+    use("windwp/nvim-spectre")
 
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
-	if PACKER_BOOTSTRAP then
-		require("packer").sync()
-	end
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if PACKER_BOOTSTRAP then
+        require("packer").sync()
+    end
 end)
