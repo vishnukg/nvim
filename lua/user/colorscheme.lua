00ad3c712c71000079c62a40a9822e65197961ee
Vishnu Ganesan <vishnukg@gmail.com>
2023-05-05 00:02:13 +1000

    Changed visual selection color

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -3,35 +3,35 @@ local colorscheme = "vscode"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-    return
+	return
 end
 
-vim.o.background = 'dark'
+vim.o.background = "dark"
 
 local c = require("vscode.colors").get_colors()
-require('vscode').setup({
-    -- Alternatively set style in setup
-    -- style = 'light'
+require("vscode").setup({
+	-- Alternatively set style in setup
+	-- style = 'light'
 
-    -- Enable transparent background
-    transparent = true,
-    -- Enable italic comment
-    italic_comments = true,
-    -- Disable nvim-tree background color
-    disable_nvimtree_bg = true,
-    -- Override colors (see ./lua/vscode/colors.lua)
-    color_overrides = {
-        vscLineNumber = "#464d53",
-    },
-    -- Override highlight groups (see ./lua/vscode/theme.lua)
-    group_overrides = {
-        -- this supports the same val table as vim.api.nvim_set_hl
-        -- use colors from this colorscheme by requiring vscode.colors!
-        Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
-    }
+	-- Enable transparent background
+	transparent = true,
+	-- Enable italic comment
+	italic_comments = true,
+	-- Disable nvim-tree background color
+	disable_nvimtree_bg = true,
+	-- Override colors (see ./lua/vscode/colors.lua)
+	color_overrides = {
+		vscLineNumber = "#464d53",
+	},
+	-- Override highlight groups (see ./lua/vscode/theme.lua)
+	group_overrides = {
+		-- this supports the same val table as vim.api.nvim_set_hl
+		-- use colors from this colorscheme by requiring vscode.colors!
+		Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
+	},
 })
 
-require('vscode').load()
+require("vscode").load()
 
 -- Transparency setting
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "none", fg = "none" })
@@ -42,7 +42,7 @@ require('vscode').load()
 -- vim.api.nvim_set_hl(0, "VertSplit", { bg = "none", fg = "none" })
 -- vim.api.nvim_set_hl(0, "SignColumn", { bg = "none", fg = "none" })
 
-vim.api.nvim_set_hl(0, "Visual", { bg = "#4da9ff", fg = "None" })
+vim.api.nvim_set_hl(0, "Visual", { bg = "#464d53", fg = "None" })
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "#464d53", fg = "None" })
 
 -- hi Pmenu bg=color to set popup menu background
