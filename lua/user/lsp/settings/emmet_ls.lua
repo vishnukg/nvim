1d63065d36ac34573f556837e726e6d63a0a11f1
Vishnu Ganesan <vishukg@gmail.com>
2023-07-24 14:38:31 +1000

    Added more control to emmet ls settings

---
+++ lua/user/lsp/settings/emmet_ls.lua
@@ -0,0 +1,12 @@
+return {
+    -- on_attach = on_attach,
+    filetypes = { "css", "eruby", "html", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
+    init_options = {
+      html = {
+        options = {
+          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+          ["bem.enabled"] = true,
+        },
+      },
+    }
+}
