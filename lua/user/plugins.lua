7b1e217331f0111b1503a386e99a9b39ae1a18d9
Vishnu Ganesan <vishnukg@gmail.com>
2023-08-25 11:51:23 +1000

    Moving from packer to lazy nvim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,141 +1,114 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system({
+--
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
 		"git",
 		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
 	})
-	print("Installing packer close and reopen Neovim...")
-	vim.cmd([[packadd packer.nvim]])
 end
-
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
+vim.opt.rtp:prepend(lazypath)
 
 -- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
+local status_ok, lazy = pcall(require, "lazy")
 if not status_ok then
 	return
 end
 
--- Have packer use a popup window
-packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
-})
-
 -- Install your plugins here
-return packer.startup(function(use)
+return lazy.setup({
 	-- Basic plugins here
-	use("wbthomason/packer.nvim") -- Have packer manage itself
-	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
-	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
-	use({
+	"nvim-lua/popup.nvim", -- An implementation of the Popup API from vim in Neovim
+	"nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
+	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.0",
 		-- or                            , branch = '0.1.x',
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
-	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
-	use("nvim-treesitter/playground")
-	use("mbbill/undotree")
-	use("tpope/vim-fugitive")
-	use("kyazdani42/nvim-web-devicons")
-	use("kyazdani42/nvim-tree.lua")
-	use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and treesitter
-	use("akinsho/toggleterm.nvim")
+		dependencies = { { "nvim-lua/plenary.nvim" } },
+	},
+	"nvim-treesitter/nvim-treesitter",
+	{ build = ":TSUpdate" },
+	"nvim-treesitter/playground",
+	"mbbill/undotree",
+	"tpope/vim-fugitive",
+	"windwp/nvim-autopairs", -- Autopairs, integrates with both cmp and treesitter
+	"akinsho/toggleterm.nvim",
 
 	-- Lualine
-	use({
+	{
 		"nvim-lualine/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-	})
+		dependencies = { "kyazdani42/nvim-web-devicons", opt = true },
+	},
 
 	-- Colorscheme
-	use("lunarvim/darkplus.nvim")
-	use("Mofiqul/vscode.nvim")
+	"lunarvim/darkplus.nvim",
+	"Mofiqul/vscode.nvim",
 
 	-- Commenting code
-	use({
+	{
 		"numToStr/Comment.nvim",
 		config = function()
 			require("Comment").setup()
 		end,
-	})
+	},
 
 	-- cmp plugins
-	use("hrsh7th/nvim-cmp") -- The completion plugin
-	use("hrsh7th/cmp-buffer") -- buffer completions
-	use("hrsh7th/cmp-path") -- path completions
-	use("hrsh7th/cmp-cmdline") -- cmdline completions
-	use("saadparwaiz1/cmp_luasnip") -- snippet completions
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-nvim-lua")
+	"hrsh7th/nvim-cmp", -- The completion plugin
+	"hrsh7th/cmp-buffer", -- buffer completions
+	"hrsh7th/cmp-path", -- path completions
+	"hrsh7th/cmp-cmdline", -- cmdline completions
+	"saadparwaiz1/cmp_luasnip", -- snippet completions
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-nvim-lua",
 
 	-- snippets
-	use("L3MON4D3/LuaSnip") --snippet engine
-	use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
+	"L3MON4D3/LuaSnip", --snippet engine
+	"rafamadriz/friendly-snippets", -- a bunch of snippets to use
 
 	-- LSP
-	use("neovim/nvim-lspconfig") -- enable LSP
-	use("williamboman/mason.nvim") -- simple to use language server installer
-	use("williamboman/mason-lspconfig.nvim") -- simple to use language server installer
-	use("jose-elias-alvarez/null-ls.nvim") -- LSP diagnostics and code actions
-	use({
+	"neovim/nvim-lspconfig", -- enable LSP
+	"williamboman/mason.nvim", -- simple to use language server installer
+	"williamboman/mason-lspconfig.nvim", -- simple to use language server installer
+	"jose-elias-alvarez/null-ls.nvim", -- LSP diagnostics and code actions
+	{
 		"j-hui/fidget.nvim",
 		tag = "legacy",
-	}) -- LSP progress status
+	}, -- LSP progress status
 
 	-- need to install efm language server from mason
-	use({
+	{
 		"creativenull/efmls-configs-nvim",
-		tag = "v1.*", -- tag is optional, but recommended
-		requires = { "neovim/nvim-lspconfig" },
-	})
+		version = "v1.x.x", -- version is optional, but recommended
+		dependencies = { "neovim/nvim-lspconfig" },
+	},
 
-	use("lukas-reineke/lsp-format.nvim")
+	"lukas-reineke/lsp-format.nvim",
 
 	-- Go language support
-	use({
+	{
 		"ray-x/go.nvim",
 		config = function()
 			require("go").setup()
 		end,
-	})
-	use({ "ray-x/guihua.lua", run = "cd lua/fzy && make" }) -- recommended if need floating window support
+	},
+	{ "ray-x/guihua.lua", build = "cd lua/fzy && make" }, -- recommended if need floating window support
 
 	-- Git
-	use("lewis6991/gitsigns.nvim")
+	"lewis6991/gitsigns.nvim",
 
 	-- Search and Replace
-	use("windwp/nvim-spectre")
+	"windwp/nvim-spectre",
 
 	-- Testing
-	use({
+	{
 		"nvim-neotest/neotest",
-		requires = {
+		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-treesitter/nvim-treesitter",
 			"antoinemadec/FixCursorHold.nvim",
 		},
-	})
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
-	if PACKER_BOOTSTRAP then
-		require("packer").sync()
-	end
-end)
+	},
+})
