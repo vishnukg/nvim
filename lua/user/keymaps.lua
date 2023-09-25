5ed9ca893c4250fba7991b9ce14912addbb32ec8
Vishnu Ganesan <vishnukg@gmail.com>
2023-09-25 12:21:56 +1000

    Update grep find telescope command and lazy updates

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -51,7 +51,7 @@ keymap("v", "p", '"_dP', opts)
 
 --Telescope mappings
 keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
-keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
+keymap("n", "<leader>fg", ":Telescope live_grep<CR>", opts)
 keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
 keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
 
