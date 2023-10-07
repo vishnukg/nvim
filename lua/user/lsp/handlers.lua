7099870fd91dbb382bd5da001fd6878fcad55bdd
Vishnu Ganesan <vishnukg@gmail.com>
2023-10-07 15:23:47 +1100

    Dont show error lines by default.

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -23,7 +23,7 @@ M.setup = function()
 	end
 
 	local config = {
-		virtual_text = true, -- enable/disable virtual text
+		virtual_text = false, -- enable/disable virtual text
 		signs = {
 			active = signs, -- show signs
 		},
