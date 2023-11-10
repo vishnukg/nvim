d5c6d612ab5d062d887b5f10ab49848571519eb6
Vishnu Ganesan <vishnukg@gmail.com>
2023-11-10 13:53:27 +1100

    Replace null ls with none ls

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -83,7 +83,7 @@ return lazy.setup({
 	"neovim/nvim-lspconfig", -- enable LSP
 	"williamboman/mason.nvim", -- simple to use language server installer
 	"williamboman/mason-lspconfig.nvim", -- simple to use language server installer
-	"jose-elias-alvarez/null-ls.nvim", -- LSP diagnostics and code actions
+	"nvimtools/none-ls.nvim",
 	{
 		"j-hui/fidget.nvim",
 		tag = "legacy",
