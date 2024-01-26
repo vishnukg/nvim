acb9ae979f6255cee5429e25059e236f37439ea6
Vishnu Ganesan <vishnukg@gmail.com>
2024-01-26 20:07:20 +0530

    Added vscode theme

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -1,7 +1,7 @@
 require("user.options")
 require("user.keymaps")
 require("user.plugins")
-require("user.onedark-color")
+require("user.colorscheme")
 require("user.cmp")
 require("user.lsp")
 require("user.telescope")
--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -6,7 +6,7 @@ end
 lualine.setup({
 	options = {
 		icons_enabled = true,
-		theme = "onedark",
+		theme = "vscode",
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
