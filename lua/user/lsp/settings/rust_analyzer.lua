da2d9236a2c9ad9e439e7a7af0e7a493c133519e
Vishnu Ganesan <vishnukg@gmail.com>
2024-02-02 18:45:17 +0530

    Updated rust-analyzer lsp settings

--- lua/user/lsp/settings/rust_analyzer.lua
+++ lua/user/lsp/settings/rust_analyzer.lua
@@ -1,13 +1,4 @@
 return {
-	tools = {
-		autoSetHints = true,
-		hover_with_actions = true,
-		inlay_hints = {
-			show_parameter_hints = true,
-			parameter_hints_prefix = "",
-			other_hints_prefix = "",
-		},
-	},
 	settings = {
 		["rust-analyzer"] = {
 			assist = {
@@ -21,11 +12,11 @@ return {
 				-- default: `cargo check`
 				command = "clippy",
 			},
-		},
-		inlayHints = {
-			lifetimeElisionHints = {
-				enable = true,
-				useParameterNames = true,
+			inlayHints = {
+				lifetimeElisionHints = {
+					enable = true,
+					useParameterNames = true,
+				},
 			},
 		},
 	},
