5f9f95ae0ed6f7a44e6733281dd81fc7d3eedc9a
Vishnu Ganesan <vishnukg@gmail.com>
2024-05-17 23:38:03 +1000

    Update vscode colors

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -6,21 +6,30 @@ if not status_ok then
 	return
 end
 
+vim.o.background = "dark"
+
 local c = require("vscode.colors").get_colors()
 require("vscode").setup({
 	-- Alternatively set style in setup
-	-- style = 'light'
+	style = "dark",
 
 	-- Enable transparent background
 	transparent = true,
+
 	-- Enable italic comment
 	italic_comments = true,
+
+	-- Underline `@markup.link.*` variants
+	underline_links = true,
+
 	-- Disable nvim-tree background color
 	disable_nvimtree_bg = true,
+
 	-- Override colors (see ./lua/vscode/colors.lua)
 	color_overrides = {
 		vscLineNumber = "#464d53",
 	},
+
 	-- Override highlight groups (see ./lua/vscode/theme.lua)
 	group_overrides = {
 		-- this supports the same val table as vim.api.nvim_set_hl
@@ -29,7 +38,7 @@ require("vscode").setup({
 	},
 })
 
-require("vscode").load()
+vim.cmd.colorscheme("vscode")
 
 -- Transparency setting
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "none", fg = "none" })
@@ -46,5 +55,3 @@ vim.api.nvim_set_hl(0, "CursorLine", { bg = "#464d53", fg = "None" })
 -- hi Pmenu bg=color to set popup menu background
 -- hi PmenuSel bg=color to set popup menu selection background
 vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#4da9ff", fg = "None" })
-
-vim.o.background = "dark"
