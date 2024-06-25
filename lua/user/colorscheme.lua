4fc7c5fed233824a3824fa596fbbf53c0d461b27
Vishnu Ganesan <vishnukg@gmail.com>
2024-06-25 21:55:06 +1000

    Remove commented lines

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -40,15 +40,6 @@ require("vscode").setup({
 
 vim.cmd.colorscheme("vscode")
 
--- Transparency setting
--- vim.api.nvim_set_hl(0, "Normal", { bg = "none", fg = "none" })
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none", fg = "none" })
--- vim.api.nvim_set_hl(0, "StatusLine", { bg = "none", fg = "none" })
--- vim.api.nvim_set_hl(0, "Folded", { bg = "none", fg = "none" })
--- vim.api.nvim_set_hl(0, "SpecialKey", { bg = "none", fg = "none" })
--- vim.api.nvim_set_hl(0, "VertSplit", { bg = "none", fg = "none" })
--- vim.api.nvim_set_hl(0, "SignColumn", { bg = "none", fg = "none" })
-
 vim.api.nvim_set_hl(0, "Visual", { bg = "#338fcc", fg = "None" })
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "#464d53", fg = "None" })
 
