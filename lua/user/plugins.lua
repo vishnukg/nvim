aa8b24cbc288d4e8ceff549c6c619d268f1223bb
Vishnu Ganesan <vishnukg@gmail.com>
2024-10-14 20:30:55 +1100

    Fix gonvim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -141,17 +141,19 @@ return lazy.setup({
 		},
 	},
 
-	-- Install Golang nvim tool
-	"ray-x/go.nvim",
-	dependencies = { -- optional packages
-		"ray-x/guihua.lua",
-		"neovim/nvim-lspconfig",
-		"nvim-treesitter/nvim-treesitter",
+	{
+		-- Install Golang nvim tool
+		"ray-x/go.nvim",
+		dependencies = { -- optional packages
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("go").setup()
+		end,
+		event = { "CmdlineEnter" },
+		ft = { "go", "gomod" },
+		build = ':lua require("go.install").update_all_sync()',
 	},
-	config = function()
-		require("go").setup()
-	end,
-	event = { "CmdlineEnter" },
-	ft = { "go", "gomod" },
-	build = ':lua require("go.install").update_all_sync()',
 })
