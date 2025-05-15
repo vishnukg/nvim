7b8ec66fd03b1ecc3a15dcd86087a2a1c2be4bcf
Vishnu Ganesan <vishnukg@gmail.com>
2025-05-15 19:59:47 +1000

    Add kulala plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,137 +1,150 @@
 --
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Use a protected call so we don't error out on first use
 local status_ok, lazy = pcall(require, "lazy")
 if not status_ok then
-	return
+    return
 end
 
 -- Install your plugins here
 return lazy.setup({
-	-- Basic plugins here
-	"nvim-lua/popup.nvim", -- An implementation of the Popup API from vim in Neovim
-	"nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.4",
-		-- or                            , branch = '0.1.x',
-		dependencies = { { "nvim-lua/plenary.nvim" } },
-	},
-	{
-		"nvim-tree/nvim-tree.lua",
-		version = "*",
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			require("nvim-tree").setup({})
-		end,
-	},
-	{ "nvim-tree/nvim-web-devicons", lazy = true },
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-	"nvim-treesitter/playground",
-	"mbbill/undotree",
-	"tpope/vim-fugitive",
-	"windwp/nvim-autopairs", -- Autopairs, integrates with both cmp and treesitter
-	"akinsho/toggleterm.nvim",
+    -- Basic plugins here
+    "nvim-lua/popup.nvim",   -- An implementation of the Popup API from vim in Neovim
+    "nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.4",
+        -- or                            , branch = '0.1.x',
+        dependencies = { { "nvim-lua/plenary.nvim" } },
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("nvim-tree").setup({})
+        end,
+    },
+    { "nvim-tree/nvim-web-devicons",     lazy = true },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    "nvim-treesitter/playground",
+    "mbbill/undotree",
+    "tpope/vim-fugitive",
+    "windwp/nvim-autopairs", -- Autopairs, integrates with both cmp and treesitter
+    "akinsho/toggleterm.nvim",
 
-	-- Lualine
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
-	},
+    -- Lualine
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
+    },
 
-	-- Colorscheme
-	"Mofiqul/vscode.nvim",
+    -- Colorscheme
+    "Mofiqul/vscode.nvim",
 
-	-- Commenting code
-	{
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup()
-		end,
-	},
+    -- Commenting code
+    {
+        "numToStr/Comment.nvim",
+        config = function()
+            require("Comment").setup()
+        end,
+    },
 
-	-- cmp plugins
-	"hrsh7th/nvim-cmp", -- The completion plugin
-	"hrsh7th/cmp-buffer", -- buffer completions
-	"hrsh7th/cmp-path", -- path completions
-	"hrsh7th/cmp-cmdline", -- cmdline completions
-	"saadparwaiz1/cmp_luasnip", -- snippet completions
-	"hrsh7th/cmp-nvim-lsp",
-	"hrsh7th/cmp-nvim-lua",
+    -- cmp plugins
+    "hrsh7th/nvim-cmp",         -- The completion plugin
+    "hrsh7th/cmp-buffer",       -- buffer completions
+    "hrsh7th/cmp-path",         -- path completions
+    "hrsh7th/cmp-cmdline",      -- cmdline completions
+    "saadparwaiz1/cmp_luasnip", -- snippet completions
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-nvim-lua",
 
-	-- snippets
-	"L3MON4D3/LuaSnip", --snippet engine
-	"rafamadriz/friendly-snippets", -- a bunch of snippets to use
+    -- snippets
+    "L3MON4D3/LuaSnip",             --snippet engine
+    "rafamadriz/friendly-snippets", -- a bunch of snippets to use
 
-	-- LSP
-	"neovim/nvim-lspconfig", -- enable LSP
-	"williamboman/mason.nvim", -- simple to use language server installer
-	"williamboman/mason-lspconfig.nvim", -- simple to use language server installer
-	"nvimtools/none-ls.nvim", -- Replacing none ls with null-ls
-	{
-		"j-hui/fidget.nvim",
-		tag = "legacy",
-	}, -- LSP progress status
+    -- LSP
+    "neovim/nvim-lspconfig",             -- enable LSP
+    "williamboman/mason.nvim",           -- simple to use language server installer
+    "williamboman/mason-lspconfig.nvim", -- simple to use language server installer
+    "nvimtools/none-ls.nvim",            -- Replacing none ls with null-ls
+    {
+        "j-hui/fidget.nvim",
+        tag = "legacy",
+    }, -- LSP progress status
 
-	-- Git
-	"lewis6991/gitsigns.nvim",
+    -- Git
+    "lewis6991/gitsigns.nvim",
 
-	-- Search and Replace
-	"windwp/nvim-spectre",
+    -- Search and Replace
+    "windwp/nvim-spectre",
 
-	-- Testing
-	{
-		"nvim-neotest/neotest",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
-			"antoinemadec/FixCursorHold.nvim",
-			"nvim-neotest/nvim-nio",
-			"nvim-neotest/neotest-jest",
-			"marilari88/neotest-vitest",
-			"nvim-neotest/neotest-go",
-		},
-		config = function()
-			require("neotest").setup({
-				adapters = {
-					require("neotest-jest"),
-					require("neotest-vitest"),
-					require("neotest-go"),
-				},
-			})
-		end,
-	},
+    -- Testing
+    {
+        "nvim-neotest/neotest",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+            "antoinemadec/FixCursorHold.nvim",
+            "nvim-neotest/nvim-nio",
+            "nvim-neotest/neotest-jest",
+            "marilari88/neotest-vitest",
+            "nvim-neotest/neotest-go",
+        },
+        config = function()
+            require("neotest").setup({
+                adapters = {
+                    require("neotest-jest"),
+                    require("neotest-vitest"),
+                    require("neotest-go"),
+                },
+            })
+        end,
+    },
 
-	-- Indent blankline
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		opts = {
-			indent = { char = "╎" },
-		},
-	},
-	-- AI plugin
-	{
-		"olimorris/codecompanion.nvim",
-		config = true,
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
-		},
-	},
+    -- Indent blankline
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        opts = {
+            indent = { char = "╎" },
+        },
+    },
+    -- AI plugin
+    {
+        "olimorris/codecompanion.nvim",
+        config = true,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+    },
+    {
+        "mistweaverco/kulala.nvim",
+        keys = {
+            { "<leader>re",  desc = "Send request" },
+            { "<leader>rea", desc = "Send all requests" },
+            { "<leader>res", desc = "Open scratchpad" },
+        },
+        ft = { "http", "rest" },
+        opts = {
+            -- your configuration comes here
+            global_keymaps = false,
+        },
+    },
 })
