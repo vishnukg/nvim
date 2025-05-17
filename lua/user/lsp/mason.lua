f91eeab08859e7094eb5a1ff86d39c4a0d5d97e2
Vishnu Ganesan <vishnukg@gmail.com>
2025-05-17 20:32:08 +1000

    added mason null ls to auto install null ls lint and format tools

--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -1,57 +1,75 @@
-local servers = {
-    "lua_ls",
-    "html",
-    "ts_ls",
-    "pyright",
-    "bashls",
-    "jsonls",
-    "gopls",
-    "yamlls",
-    "emmet_ls",
-    "taplo",
-    "terraformls",
-    "clangd",
-    "omnisharp",
+local lint_and_format = {
+	-- Linters
+	"pylint",
+	"revive",
+	"stylelint",
+	"yamllint",
+	-- Formatters
+	"black",
+	"clang-format",
+	"csharpier",
+	"prettierd",
+	"stylua",
+	"yamlfmt",
+}
+
+local lsp_servers = {
+	"lua_ls",
+	"html",
+	"ts_ls",
+	"pyright",
+	"bashls",
+	"jsonls",
+	"gopls",
+	"yamlls",
+	"emmet_ls",
+	"taplo",
+	"terraformls",
+	"clangd",
+	"omnisharp",
 }
 
 local settings = {
-    ui = {
-        border = "none",
-        icons = {
-            package_installed = "✓",
-            package_pending = "⏳",
-            package_uninstalled = "✗",
-        },
-    },
-    log_level = vim.log.levels.INFO,
-    max_concurrent_installers = 4,
+	ui = {
+		border = "none",
+		icons = {
+			package_installed = "✓",
+			package_pending = "⏳",
+			package_uninstalled = "✗",
+		},
+	},
+	log_level = vim.log.levels.INFO,
+	max_concurrent_installers = 4,
 }
 
 require("mason").setup(settings)
+require("mason-null-ls").setup({
+	ensure_installed = lint_and_format,
+})
 require("mason-lspconfig").setup({
-    ensure_installed = servers,
-    automatic_installation = true,
-    automatic_enable = false,
+	ensure_installed = lsp_servers,
+	automatic_installation = true,
+	automatic_enable = false,
 })
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status_ok then
-    return
+	return
 end
 
 local opts = {}
 
-for _, server in pairs(servers) do
-    opts = {
-        on_attach = require("user.lsp.handlers").on_attach,
-        capabilities = require("user.lsp.handlers").capabilities,
-    }
-    server = vim.split(server, "@")[1]
+for _, server in pairs(lsp_servers) do
+	opts = {
+		on_attach = require("user.lsp.handlers").on_attach,
+		capabilities = require("user.lsp.handlers").capabilities,
+	}
+	server = vim.split(server, "@")[1]
 
-    local require_ok, conf_opts = pcall(require, "user.lsp.settings." .. server)
-    if require_ok then
-        opts = vim.tbl_deep_extend("force", conf_opts, opts)
-    end
+	local require_ok, conf_opts = pcall(require, "user.lsp.settings." .. server)
+	if require_ok then
+		opts = vim.tbl_deep_extend("force", conf_opts, opts)
+	end
 
-    lspconfig[server].setup(opts)
+	lspconfig[server].setup(opts)
 end
