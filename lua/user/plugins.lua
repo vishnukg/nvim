c2d83884953799215c5857e0c5c17bb9e087e16b
Vishnu Ganesan <vishnukg@gmail.com>
2025-06-10 08:13:03 +1000

    Also added codecompanion

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -147,4 +147,12 @@ return lazy.setup({
 		},
 		-- See Commands section for default commands if you want to lazy load on them
 	},
+	{
+		"olimorris/codecompanion.nvim",
+		opts = {},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+	},
 })
