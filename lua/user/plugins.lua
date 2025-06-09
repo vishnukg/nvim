36b14da06a2ea4c21d5ac7de5706320b68ec4169
Vishnu Ganesan <vishnukg@gmail.com>
2025-06-10 07:31:25 +1000

    Added copilot chat and copilot

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -136,11 +136,15 @@ return lazy.setup({
 	},
 	-- AI plugin
 	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		event = "InsertEnter",
-		config = function()
-			require("copilot").setup({})
-		end,
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			-- See Configuration section for options
+		},
+		-- See Commands section for default commands if you want to lazy load on them
 	},
 })
