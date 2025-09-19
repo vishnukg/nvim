e264e99cd9f0c872a55212fa62100754d997d088
Vishnu Ganesan <vishnukg@gmail.com>
2025-09-19 21:59:01 +1000

    reverting to old style

--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -1,12 +1,11 @@
--- Linting/formatting tools
 local lint_and_format = {
-	--linters
+	-- Linters
 	"pylint",
 	"revive",
 	"stylelint",
 	"yamllint",
 	"eslint_d",
-	--formatters
+	-- Formatters
 	"black",
 	"csharpier",
 	"prettierd",
@@ -14,7 +13,6 @@ local lint_and_format = {
 	"yamlfmt",
 }
 
--- LSP servers
 local lsp_servers = {
 	"lua_ls",
 	"html",
@@ -30,8 +28,7 @@ local lsp_servers = {
 	"csharp_ls",
 }
 
--- Mason setup
-require("mason").setup({
+local settings = {
 	ui = {
 		border = "none",
 		icons = {
@@ -42,39 +39,37 @@ require("mason").setup({
 	},
 	log_level = vim.log.levels.INFO,
 	max_concurrent_installers = 4,
-})
+}
 
--- Mason-null-ls
+require("mason").setup(settings)
 require("mason-null-ls").setup({
 	ensure_installed = lint_and_format,
 	automatic_installation = true,
 })
-
--- Mason-lspconfig
 require("mason-lspconfig").setup({
 	ensure_installed = lsp_servers,
 	automatic_installation = true,
 	automatic_enable = false,
 })
 
--- Setup LSP servers using the new vim.lsp.config API
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status_ok then
 	return
 end
 
-local default_opts = {
-	on_attach = require("user.lsp.handlers").on_attach,
-	capabilities = require("user.lsp.handlers").capabilities,
-}
+local opts = {}
 
-for _, server in ipairs(lsp_servers) do
+for _, server in pairs(lsp_servers) do
+	opts = {
+		on_attach = require("user.lsp.handlers").on_attach,
+		capabilities = require("user.lsp.handlers").capabilities,
+	}
 	server = vim.split(server, "@")[1]
 
-	local ok, server_opts = pcall(require, "user.lsp.settings." .. server)
-	if ok then
-		default_opts = vim.tbl_deep_extend("force", default_opts, server_opts)
+	local require_ok, conf_opts = pcall(require, "user.lsp.settings." .. server)
+	if require_ok then
+		opts = vim.tbl_deep_extend("force", conf_opts, opts)
 	end
 
-	lspconfig[server].setup(default_opts)
+	lspconfig[server].setup(opts)
 end
