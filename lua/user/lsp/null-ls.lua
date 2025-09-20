0563e8608b9156c6f994007da94a1cc1f8c58f3f
Vishnu Ganesan <vishnukg@gmail.com>
2025-09-20 10:43:17 +1000

    Improved nulls handlers

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -11,18 +11,18 @@ local diagnostics = null_ls.builtins.diagnostics
 -- LspFormatting
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
--- This is where you add more formatters. If you want to add a formatter, first install the binary (Ex. stylua or black)
--- to the system and then setup it up here so null-ls can use it behind the scenes.
--- Behind the scenes its uses lua vim.lsp.buf.format to autoformat
-
--- ***** You can install prettierd using Mason.
--- If you want to activate prettierd only if the prettier file is there
--- then do the following under sources
---  		formatting.prettierd.with({
--- 			condtion = function(utils)
--- 				return utils.has_file({ ".prettierrc.js" })
--- 			end,
--- 		}),
+local function lsp_format_on_save(client, bufnr)
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				vim.lsp.buf.format({ async = false })
+			end,
+		})
+	end
+end
 
 null_ls.setup({
 	debug = false,
@@ -38,21 +38,5 @@ null_ls.setup({
 		diagnostics.yamllint,
 		require("none-ls.diagnostics.eslint"),
 	},
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-					vim.lsp.buf.format({ async = false })
-				end,
-			})
-		end
-	end,
+	on_attach = lsp_format_on_save,
 })
-
--- Auto formatting on save
--- vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format({async = true})]])
---
--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,47 +1,50 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 
 if not status_ok then
-    return
+	return
 end
 
 configs.setup({
-    ensure_installed = {
-        "bash",
-        "c",
-        "javascript",
-        "json",
-        "lua",
-        "python",
-        "typescript",
-        "tsx",
-        "css",
-        "rust",
-        "yaml",
-        "markdown",
-        "markdown_inline",
-        "vim",
-        "go",
-        "c_sharp",
-        "html",
-        "xml",
-        "toml",
-        "hcl",
-        "perl",
-        "http",
-    },                    -- one of "all" or a list of languages
-    sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-    ignore_install = { "" }, -- List of parsers to ignore installing
-    autopairs = {
-        enable = true,
-    },
-    highlight = {
-        enable = true, -- false will disable the whole extension
-        disable = { "" }, -- list of language that will be disabled
-        additional_vim_regex_highlighting = false,
-    },
-    indent = { enable = true, disable = { "yaml", "html" } },
-    context_commentstring = {
-        enable = true,
-        enable_autocmd = false,
-    },
+	ensure_installed = {
+		"bash",
+		"c",
+		"javascript",
+		"json",
+		"lua",
+		"python",
+		"typescript",
+		"tsx",
+		"css",
+		"rust",
+		"yaml",
+		"markdown",
+		"markdown_inline",
+		"vim",
+		"go",
+		"c_sharp",
+		"html",
+		"xml",
+		"toml",
+		"hcl",
+		"perl",
+		"http",
+		"diff",
+	}, -- one of "all" or a list of languages
+	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+	ignore_install = { "" }, -- List of parsers to ignore installing
+	modules = {}, -- Add this line to satisfy diagnostics
+	auto_install = false, -- Add this line to satisfy diagnostics
+	autopairs = {
+		enable = true,
+	},
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		disable = { "" }, -- list of language that will be disabled
+		additional_vim_regex_highlighting = false,
+	},
+	indent = { enable = true, disable = { "yaml", "html" } },
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
 })
