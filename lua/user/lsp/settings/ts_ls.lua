c50b5f45f2a687df1c3dd7c7be3ea3528c90b77f
Vishnu Ganesan <vishnukg@gmail.com>
2025-09-20 10:22:24 +1000

    Enhance ts ls config

--- lua/user/lsp/settings/ts_ls.lua
+++ lua/user/lsp/settings/ts_ls.lua
@@ -2,6 +2,17 @@ return {
 	init_options = {
 		preferences = {
 			disableSuggestions = false,
+			includeCompletionsForModuleExports = true,
+			includeCompletionsWithInsertText = true,
+		},
+		inlayHints = {
+			includeInlayParameterNameHints = "all", -- or "none", "literals"
+			includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+			includeInlayFunctionParameterTypeHints = true,
+			includeInlayVariableTypeHints = true,
+			includeInlayPropertyDeclarationTypeHints = true,
+			includeInlayFunctionLikeReturnTypeHints = true,
+			includeInlayEnumMemberValueHints = true,
 		},
 	},
 }
