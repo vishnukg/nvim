69ac580539385c401f170579641d98e3154f58ea
Vishnu Ganesan <vishnukg@gmail.com>
2025-12-30 21:59:44 +1100

    Update telescope

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -25,9 +25,8 @@ return lazy.setup({
 	"nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.4",
-		-- or                            , branch = '0.1.x',
-		dependencies = { { "nvim-lua/plenary.nvim" } },
+		tag = "v0.2.0",
+		dependencies = { "nvim-lua/plenary.nvim" },
 	},
 	{
 		"nvim-tree/nvim-tree.lua",
