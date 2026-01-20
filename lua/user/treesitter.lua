cfc22e3bb345f6d76a897a3a996074bc4b845e51
Vishnu Ganesan <vishnukg@gmail.com>
2026-01-20 06:40:09 +0530

    Treesitter

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,66 +1,55 @@
--- Configure nvim-treesitter (rewrite version)
+-- Configure nvim-treesitter
 -- Following official docs: https://github.com/nvim-treesitter/nvim-treesitter
 
--- Filetypes to enable treesitter for
-local enabled_filetypes = {
-	"bash",
-	"c",
-	"cpp",
-	"cs",
-	"css",
-	"diff",
-	"go",
-	"hcl",
-	"html",
-	"http",
-	"javascript",
-	"javascriptreact",
-	"json",
-	"lua",
-	"markdown",
-	"perl",
-	"python",
-	"rust",
-	"sh",
-	"terraform",
-	"toml",
-	"typescript",
-	"typescriptreact",
-	"vim",
-	"xml",
-	"yaml",
-}
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+	return
+end
 
-local augroup = vim.api.nvim_create_augroup("TreesitterConfig", { clear = true })
-
--- Enable treesitter highlighting (per docs)
-vim.api.nvim_create_autocmd("FileType", {
-	group = augroup,
-	pattern = enabled_filetypes,
-	callback = function()
-		vim.treesitter.start()
-	end,
+configs.setup({
+	ensure_installed = {
+		"bash",
+		"c",
+		"c_sharp",
+		"css",
+		"diff",
+		"go",
+		"hcl",
+		"html",
+		"http",
+		"javascript",
+		"json",
+		"lua",
+		"markdown",
+		"markdown_inline",
+		"perl",
+		"python",
+		"rust",
+		"toml",
+		"tsx",
+		"typescript",
+		"vim",
+		"xml",
+		"yaml",
+	},
+	auto_install = true,
+	highlight = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+		disable = { "yaml", "html" },
+	},
 })
 
--- Enable treesitter folding (per docs)
+-- Enable treesitter folding
+local augroup = vim.api.nvim_create_augroup("TreesitterConfig", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
 	group = augroup,
-	pattern = enabled_filetypes,
+	pattern = "*",
 	callback = function()
 		vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
 		vim.wo[0][0].foldmethod = "expr"
 		vim.wo[0][0].foldlevel = 99
-	end,
-})
-
--- Enable treesitter indentation (experimental, per docs)
--- Skip yaml and html as they work better with default indentation
-vim.api.nvim_create_autocmd("FileType", {
-	group = augroup,
-	pattern = vim.tbl_filter(function(ft)
-		return ft ~= "yaml" and ft ~= "html"
-	end, enabled_filetypes),
-	callback = function()
-		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 	end,
 })
