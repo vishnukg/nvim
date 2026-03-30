62b1dabf61d5f59da16121a145a56a908d5fd35f
vishnu ganesan <vishnukg@gmail.com>
2026-03-30 15:16:48 +1100

    Fix warning

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -12,7 +12,7 @@ local diagnostics = null_ls.builtins.diagnostics
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local function lsp_format_on_save(client, bufnr)
-	if client.supports_method("textDocument/formatting") then
+	if client:supports_method("textDocument/formatting") then
 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 		vim.api.nvim_create_autocmd("BufWritePre", {
 			group = augroup,
